73ea308e2cb6788614fe0df689b754563a54adfa
Daniel Baraniak <230443@edu.p.lodz.pl>
2022-01-24 17:41:26 +0100

    Add corine service

--- corine_service.py
+++ corine_service.py
@@ -1,6 +1,6 @@
 import requests
-import json
 from math import cos
+from collections import Counter
 
 URL = "https://image.discomap.eea.europa.eu/arcgis/rest/services/Corine/CLC2018_WM/MapServer/identify"
 BASE_PARAMS = {
@@ -16,23 +16,81 @@ BASE_PARAMS = {
 def get_corine_data(x, y, diameter=10000):
     payload = BASE_PARAMS
 
-    map_extend_y = 110574 / diameter / 2
-    map_extend_x = 111320 * cos(x) / diameter / 2
-
-    mapExtend = ",".join(
-        (
-            str(y - map_extend_y),
-            str(x - map_extend_x),
-            str(y + map_extend_y),
-            str(x + map_extend_x),
-        )
-    )
-    payload["mapExtent"] = mapExtend
-    payload["geometry"] = ",".join((str(y), str(x)))
+    payload["mapExtent"] = ",".join([str(i) for i in get_envelope(x, y, diameter)])
+    payload["geometry"] = ",".join((str(x), str(y)))
 
     response = requests.get(URL, params=payload)
+
+    result = ()
+
     try:
         attributes = response.json()["results"][0]["attributes"]
-    except IndexError as e:
-        return "", ""
-    return (attributes["LABEL3"], attributes["CODE_18"])
+        result = (
+            attributes["CODE_18"],
+            attributes["LABEL3"],
+        )
+    except Exception:
+        return None
+    return result
+
+
+def get_envelope(x, y, diameter=10000):
+    map_extend_y = (diameter / 110574) / 2
+    map_extend_x = (diameter / (111320 * cos(x))) / 2
+    return (
+        x - map_extend_x,
+        y - map_extend_y,
+        x + map_extend_x,
+        y + map_extend_y,
+    )
+
+
+def get_corine_data_list(x, y, diameter=10000):
+
+    results = []
+
+    for dia in [diameter / 10, diameter / 3, diameter / 3 * 2]:
+        a, b, c, d = get_envelope(x, y, dia)
+        r = [
+            get_corine_data(a, b, dia),
+            get_corine_data(c, d, dia),
+            get_corine_data(a, d, dia),
+            get_corine_data(c, b, dia),
+            get_corine_data(x, b, dia),
+            get_corine_data(x, d, dia),
+            get_corine_data(a, y, dia),
+            get_corine_data(c, y, dia),
+        ]
+
+        results.extend(r)
+
+    results.append(get_corine_data(x, y))
+
+    return results
+
+
+def get_corine_data_types(x, y, diameter=10000):
+
+    l = get_corine_data_list(x, y, diameter)
+    if l[0] == None:
+        return None
+
+    return ",".join([x[1] for x in set(l)])
+
+
+def get_latest_corine_label(x, y, diameter=10000):
+    l = get_corine_data_list(x, y, diameter)
+
+    if l[0] == None:
+        return None
+
+    max_no_500_key = 0
+    label = ""
+
+    for k, v in l:
+        k = int(k)
+        if k > max_no_500_key and k < 500:
+            max_no_500_key = k
+            label = v
+
+    return label
