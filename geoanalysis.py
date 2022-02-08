52d1b99e0f2b0499a69801a3700bd8fbfad802d5
Daniel Baraniak <230443@edu.p.lodz.pl>
2022-02-08 22:19:07 +0100

    add preview

--- geoanalysis.py
+++ geoanalysis.py
@@ -5,6 +5,7 @@ from geopandas import GeoDataFrame
 from datetime import datetime, timedelta
 import movingpandas as mpd
 import glob
+import corine_service as cs
 
 
 def import_data(data_directory_path):
@@ -12,25 +13,67 @@ def import_data(data_directory_path):
 
     li = []
 
+    if len(all_files) == 0:
+        return None
+
     for filename in all_files:
         df = pd.read_csv(filename, index_col=None, header=0, sep=";")
         li.append(df)
 
     df = pd.concat(li, axis=0, ignore_index=True)
 
-
     # clean data
     df = df[(df.satcount != 0)]
-    df['t'] = pd.to_datetime(df['UTC_datetime'], format='%d.%m.%Y %H:%M')
-    df = df.drop(columns=['datatype', 'UTC_datetime', 'UTC_date', 'satcount', 'U_bat_mV', 'bat_soc_pct', 'solar_I_mA', 'hdop', 'mag_x',	'mag_y', 'mag_z', 'acc_x','acc_y','acc_z'])
-    df = df.set_index('t').tz_localize(None)
-    df = df.sort_values(by=['t'])
-    df = GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude), crs=4326)
+    df["t"] = pd.to_datetime(df["UTC_datetime"], format="%d.%m.%Y %H:%M")
+    df = df.drop(
+        columns=[
+            "datatype",
+            "UTC_datetime",
+            "UTC_date",
+            "satcount",
+            "U_bat_mV",
+            "bat_soc_pct",
+            "solar_I_mA",
+            "hdop",
+            "mag_x",
+            "mag_y",
+            "mag_z",
+            "acc_x",
+            "acc_y",
+            "acc_z",
+        ]
+    )
+    df = df.set_index("t").tz_localize(None)
+    df = df.sort_values(by=["t"])
+    df = GeoDataFrame(
+        df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude), crs=4326
+    )
 
     return df
 
+
 def get_trajectory_collection(df):
-    return mpd.TrajectoryCollection(df, 'device_id', t='t')
+    return mpd.TrajectoryCollection(df, "device_id", t="t")
+
 
 def get_device_ids(df):
     return df.device_id.unique()
+
+
+def detect_stops(tc, min_duration_h, max_diameter):
+
+    min_duration = timedelta(hours=min_duration_h)
+
+    stops = mpd.TrajectoryStopDetector(tc).get_stop_points(
+        min_duration=min_duration, max_diameter=max_diameter
+    )
+
+    stops = stops.assign(duration_h=stops.duration_s / (60 * 60))
+    stops = stops.drop(columns=["duration_s"])
+
+    stops[["corine_label_id", "corine_label_text"]] = stops.apply(
+        lambda row: cs.get_corine_data(x=row["geometry"].x, y=row["geometry"].y),
+        axis=1,
+        result_type="expand",
+    )
+    return stops
--- main.py
+++ main.py
@@ -3,24 +3,79 @@ import geoanalysis as g
 import pandas as pd
 
 # sg.theme("DarkAmber")
-df = g.import_data("data")
+
+df = pd.DataFrame([])
+tc = []
 
 layout = [
     [sg.Text("Folder z plikami csv")],
-    [sg.Input(key="dirname"), sg.FolderBrowse()],
+    [sg.Input(key="dirname", default_text="data"), sg.FolderBrowse()],
     [sg.Button("Otwórz"), sg.Button("Anuluj")],
     [
         sg.Table(
             values=df,
-            headings=list(df.columns.values),
+            headings=[
+                "device_id",
+                "UTC_datetime",
+                "Latitude",
+                "Longitude",
+                "Altitude_m",
+                "speed_km_h",
+                "direction_deg",
+                "temperature_C",
+            ],
+            vertical_scroll_only=False,
             visible=False,
-            key="table",
+            key="-PREVIEW_TABLE-",
         ),
         sg.Listbox(
-            values=["Listbox 1", "Listbox 2", "Listbox 3"],
+            values=[],
             size=(30, 6),
+            select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE,
             visible=False,
             key="deviceIdListbox",
+        ),
+    ],
+    [
+        sg.Text("Średnica obszaru:", size=20),
+        sg.Input(
+            key="-MAX_DIAMETER-",
+            default_text=10000,
+            size=6,
+            justification="right",
+            enable_events=True,
+        ),
+        sg.Text("m"),
+    ],
+    [
+        sg.Text("Minimalny czas postoju:", size=20),
+        sg.Input(
+            key="-MIN_DURATION-",
+            default_text=48,
+            size=6,
+            justification="right",
+            enable_events=True,
+        ),
+        sg.Text("h"),
+    ],
+    [
+        sg.Button("Znajdź przystanki", key="stopDetect", disabled=True),
+    ],
+    [
+        sg.Table(
+            values=df,
+            headings=[
+                "geometry",
+                "start_time",
+                "end_time",
+                "traj_id",
+                "duration_h",
+                "corine_label_id",
+                "corine_label_text",
+            ],
+            vertical_scroll_only=False,
+            visible=False,
+            key="-STOPS_TABLE-",
         ),
     ],
 ]
@@ -30,17 +85,42 @@ window = sg.Window("Window Title", layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
+
     if (
         event == sg.WIN_CLOSED or event == "Cancel"
     ):  # if user closes window or clicks cancel
         break
+
     if event == "Otwórz" and values["dirname"] != "":
         df = g.import_data(data_directory_path=values["dirname"])
-        window["table"].update(
+        window["-PREVIEW_TABLE-"].update(
             visible=True,
             values=list(df.values),
         )
         window["deviceIdListbox"].update(visible=True, values=g.get_device_ids(df))
+        window["stopDetect"].update(disabled=False)
+
+    if event == "stopDetect":
+        tc = g.get_trajectory_collection(df)
+        stops = g.detect_stops(
+            tc,
+            min_duration_h=int(values["-MIN_DURATION-"]),
+            max_diameter=int(values["-MAX_DIAMETER-"]),
+        )
+        window["-STOPS_TABLE-"].update(visible=True, values=list(df.values))
+
+    # if (
+    #     event == "-MAX_DIAMETER-"
+    #     and values["-MAX_DIAMETER-"]
+    #     and values["-MAX_DIAMETER-"][-1] not in ("0123456789")
+    # ):
+    #     window["-MAX_DIAMETER-"].update(values["-MAX_DIAMETER-"][:-1])
+    # if (
+    #     event == "-MIN_DURATION-"
+    #     and values["-MIN_DURATION-"]
+    #     and values["-MIN_DURATION-"][-1] not in ("0123456789")
+    # ):
+    #     window["-MIN_DURATION-"].update(values["-MIN_DURATION-"][:-1])
 
 
 window.close()
